fc925f01dfc63840cbafd3f8ca5d0bebbb93b7fc
Altemir Pacheco <altemir.pacheco@gmail.com>
2023-04-09 16:01:31 -0300

    Modulo 3

--- vue-composition-api-basics/src/views/HomeView.vue
+++ vue-composition-api-basics/src/views/HomeView.vue
@@ -1,5 +1,79 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
-  </div>
+    <div class="home">
+        <h1>Home</h1>
+        <div>
+            <button class="btn" @click="decreaseCounter">-</button>
+            <span class="counter">{{ counter }}</span>
+            <button class="btn" @click="increaseCounter">+</button>
+        </div>
+    </div>
 </template>
+
+<script setup>
+import {ref} from "vue";
+
+const counter = ref(0)
+const increaseCounter = () => {
+    counter.value++
+}
+const decreaseCounter = () => {
+    counter.value--
+}
+
+
+</script>
+
+<!--Composition API-->
+<!--<script>-->
+<!--    import {ref} from "vue";-->
+
+<!--    export default {-->
+<!--        setup(){-->
+<!--            const counter = ref(0)-->
+<!--            const increaseCounter = ()=> {-->
+<!--                counter.value++-->
+<!--            }-->
+<!--            const decreaseCounter = ()=> {-->
+<!--                counter.value&#45;&#45;-->
+<!--            }-->
+
+<!--            return{-->
+<!--                counter,-->
+<!--                increaseCounter,-->
+<!--                decreaseCounter-->
+<!--            }-->
+<!--        }-->
+<!--    }-->
+<!--</script>-->
+
+<!--Options API-->
+<!--<script>-->
+<!--export default {-->
+<!--    data() {-->
+<!--        return {-->
+<!--            counter: 0-->
+<!--        }-->
+<!--    },-->
+<!--    methods: {-->
+<!--        increaseCounter() {-->
+<!--            this.counter++-->
+<!--        },-->
+<!--        decreaseCounter() {-->
+<!--            this.counter&#45;&#45;-->
+<!--        }-->
+<!--    }-->
+<!--}-->
+<!--</script>-->
+
+
+<style scoped>
+.home {
+    text-align: center;
+    padding: 20px;
+}
+
+.btn, .counter {
+    font-size: 40px;
+    margin: 10px;
+}
+</style>
